c883e03f8a3b55722c6e116a81d58669335adc37
oxy <oxy@220.116.174.14>
2018-07-16 21:54:26 +0900

    숙숙제제

--- ict_report/src/kks/Test.java
+++ ict_report/src/kks/Test.java
@@ -1,5 +1,46 @@
 package kks;
 
 public class Test {
+	public static void main(String[] args) {
+		Integer i = 5;
+		
+//		char ch = '5';
+//		System.out.println(i.equals(ch));
+//		
+//		int is = i.intValue();
+//		System.out.println(is);
+//		
+//		long ln = i.longValue();
+//		System.out.println(ln);
+		
+		Integer a = 5;
+		int b = a.parseInt("1B", 16);
+		System.out.println(b);
+		
+		
+		
+		
+		
+		
+		// for (int i = 1; i <= 10; i++) {
+		// System.out.println(i);
+		//
+		// }
+		//
+		// for (int i = 10; 0 < i; i--) {
+		// if (i % 2 == 0) {
+		// System.out.println(i);
+		// }
+		//
+		// }
+		//
+		// for (int i = 10; 0 < i; i--) {
+		// if (i % 2 == 1) {
+		// System.out.println(i);
+		// }
+		//
+		// }
+
+	}
 
 }
