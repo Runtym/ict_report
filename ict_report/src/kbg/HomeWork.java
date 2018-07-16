56900544e6cceba58a5a817ce82d27cc70c17bf6
kbg <kbg@DESKTOP-IDGE35M>
2018-07-16 16:20:24 +0900

    HomeWork

---
+++ ict_report/src/kbg/HomeWork.java
@@ -0,0 +1,36 @@
+//
+//1. 반복문으로 1부터 10까지 찍는 소스를 작성하세요
+//  (클래스 main함수까티 써야 합니다.)
+//
+//2. 반복문으로 10부터 1까지 짝수만 찍는 코드를 작성하세요.
+//
+//3. 반복문으로 10부터 1까지 홀수만 찍는 코드를 작성하세요.
+//
+//
+
+
+package kbg;
+
+public class HomeWork {
+	
+	public static void main(String[] args) {
+		System.out.println("1번 지문");
+		for(int i =1;i<=10;i++) {
+			System.out.print(i);
+		}
+		System.out.println("2번 지문");
+		for(int i =10;i>=1;i--) {
+			if(i%2==0) {
+				System.out.print(i);
+			}
+		}
+		System.out.println("3번 지문");
+		for(int i =10;i>=1;i--) {
+			if(i%3==0||i==1) {
+				System.out.print(i);
+			}
+		}
+			
+	}
+}
+	
