3bd43dfe37d39783a980832cb0ec9ae2236d1e0e
koitt03-A <koitt03-A@koitt03-A-PC>
2018-07-16 16:53:41 +0900

    180716

--- ict_report/src/zhk/Test.java
+++ ict_report/src/zhk/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("1. 클래스 Father와 Son이 있다면 Son에서 Father를 extends 하면 Father가 가지고 있는 모든 것을 상속 받아 오버라이딩을 할 수 있다.");
-		System.out.println("2. 오버로딩은 다른 매개 변수를 같은 이름의 메소드에 넣어 여러개를 가질 수 있게 하는 것입니다.");
+		System.out.println("2. 오버로딩은 다른 매개 변수를 같은 이름의 메소드에 넣어 같은 이름의 메소드를 여러개를 가질 수 있게 하는 것입니다.");
 		System.out.println("3. 부모클래스가 있을 때 자식클래스가 부모의 메서드를 재정의할 수 있는 것입니다.");
 		System.out.println("4. 메서드는 객체의 초기화를 위해 쓰이며 리턴이 없고, 생성자와 이름이 같아야 한다.");
 		
