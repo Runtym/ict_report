415f0868f5e22b40bab30a801e57883b47693114
koitt03-A <koitt03-A@koitt03-A-PC>
2018-07-23 10:32:38 +0900

    asdf

---
+++ ict_report/src/phw/phw20180720/test26/UserDAO.java
@@ -0,0 +1,13 @@
+package phw.phw20180720.test26;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface UserDAO {
+
+	public ArrayList<HashMap<String, String>> selectUserList(HashMap<String, String> user);
+	public int insertUserList(HashMap<String, String> user);
+	public int updateUserList(HashMap<String, String> user);
+	public int deleteUserList(HashMap<String, String> user);
+	
+} 
