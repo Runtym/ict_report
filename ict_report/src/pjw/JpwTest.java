37b7ec72be6e39966361a81faff5ff253231e227
koitt03-A <koitt03-A@koitt03-A-PC>
2018-07-26 16:02:02 +0900

    더 열심히 하겠습니다.

---
+++ ict_report/src/pjw/JpwTest.java
@@ -0,0 +1,85 @@
+package pjw;
+
+public class JpwTest {
+
+//public class test {
+//2. 다음 빈칸에 들어갈 코드를 적으세요.
+//public static void main(String[] args) {
+//		int num = 63;
+//		
+//		if(   num==63     ) {
+//			System.out.println("num이 100 미만입니다.");
+//		} else if(     num==63    ){
+//			System.out.println("num이 100 초과입니다. ");
+//		} else {
+//			System.out.println("num이 100입니다.");
+//		}
+//		
+//	}
+//}
+//5. 1부터 50까지 중에서 3의 배수이고 7의배수인 숫자를 출력해주세요. 
+//public static void main(String[] args) {
+//for(int i=1; i<50; i++){
+//if(i%3==0){
+//System.out.println(i);
+//}
+//
+//public static void main(String[] args){
+//for(int j=1; j<50; j++){
+//if(j%7==0){
+//System.out.println(j);
+//}
+//}
+//}
+
+//8. 다음 프로그램에 실행 결과를 쓰세요.  (내가 달립니다.)
+//
+//class Father{
+//
+//public void run() {
+//	System.out.println("아빠가 달립니다.");
+//}
+//}
+//
+//public class Test extends Father{
+//
+//@Override
+//public void run() {
+//	System.out.println("내가 달립니다.");
+//}
+//
+//public static void main(String[] args) {
+//	Father f = new Test();
+//	f.run();
+//
+//	Father f2 = new Father();
+//	f2.run();
+//}
+//
+//}
+//	10. 방금만든 user_info 테이블의 내용을 모두 보고 싶을때에 사용하는 명령어를 쓰세요.
+//	select*from user_info;
+	
+//
+// 9. user_info 라는 이름, 나이, 주소를 저장할 수 있는 table을 만드는 명령어를 쓰세요 
+
+//create database boom;
+//use boom;
+//create table user_info(
+//uNum int AUTO_INCREMENT PRIMARY key,
+//uName varchar(100) not null,
+//uAge tinyint not null,
+//uAddress varchar(200) not null,
+//);
+//
+//insert into user_info
+//(uName, uAge, uAddress, uEtc)
+//values
+//('이윤열',40,'도쿄'),
+//('홍진호',35,'도쿄'),
+//('김연아',22,'서울');
+//
+//select * from user_info
+//}
+
+
