e2e624b4ad53e94ea5cb7b9a7a939fa14a9dbe98
Hikari <Hikari@Hikari>
2018-07-26 14:33:14 +0900

    乃

--- ict_report/src/kmw/Exam_07_26.java
+++ ict_report/src/kmw/Exam_07_26.java
@@ -2,25 +2,7 @@ package kmw;
 
 import java.util.ArrayList;
 
-class Father{
-	
-	public void run() {
-		System.out.println("아빠가 달립니다.");
-	}
-}
+public class Exam_07_26{
 
-public class Exam_07_26 extends Father{
 
-	@Override
-	public void run() {
-		System.out.println("내가 달립니다.");
-	}
-
-	public static void main(String[] args) {
-		Father f = new Exam_07_26();
-		f.run();
- 
-		Father f2 = new Father();
-		f2.run();
-	}
 }
