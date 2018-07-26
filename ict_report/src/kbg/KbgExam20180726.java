47c23648d88be3f49f8e496475c4d48a603c1314
kbg <kbg@DESKTOP-IDGE35M>
2018-07-26 14:39:13 +0900

    adsfasf

--- ict_report/src/kbg/KbgExam20180726.java
+++ ict_report/src/kbg/KbgExam20180726.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class KbgExam20180726 {
-//	1. 臾몄옄?뿴 2媛쒕?? ?엯?젰諛쏆븘 ?젙?닔濡? 蹂??솚?븯?뿬 ?몢?닔?쓽 ?빀?쓣 異쒕젰?븯?뒗 ?봽濡쒓렇?옩?쓣 ?옉?꽦?빐二쇱꽭?슂.
+//	1. 문자열 2개를 입력받아 정수로 변환하여 두수의 합을 출력하는 프로그램을 작성해주세요.
 //	public static void main(String[] args) {
 //		Scanner scan = new Scanner(System.in);
-//		System.out.println("?뜑?븯?떎 ?닽?옄 a瑜? ?엯?젰?븯?꽭?슂");
+//		System.out.println("더하실 숫자 a를 입력하세요");
 //		int a = Integer.parseInt(scan.nextLine());
-//		System.out.println("?뜑?븯?떎 ?닽?옄 b瑜? ?엯?젰?븯?꽭?슂");
+//		System.out.println("더하실 숫자 b를 입력하세요");
 //		int b = Integer.parseInt(scan.nextLine());
 //		System.out.println(a+b);
 //	}
 
-//	2. ?떎?쓬 鍮덉뭏?뿉 ?뱾?뼱媛? 肄붾뱶瑜? ?쟻?쑝?꽭?슂.
+//	2. 다음 빈칸에 들어갈 코드를 적으세요.
 //
 //	public class Test {
 //
@@ -22,18 +22,18 @@ public class KbgExam20180726 {
 //			int num = 63;
 //			
 //			if(   num<100     ) {
-//				System.out.println("num?씠 100 誘몃쭔?엯?땲?떎.");
+//				System.out.println("num이 100 미만입니다.");
 //			} else if(     num>100     ){
-//				System.out.println("num?씠 100 珥덇낵?엯?땲?떎. ");
+//				System.out.println("num이 100 초과입니다. ");
 //			} else {
-//				System.out.println("num?씠 100?엯?땲?떎.");
+//				System.out.println("num이 100입니다.");
 //			}
 //			
 //		}
 //	}
 
-//	3. ?젙?닔?삎 諛곗뿴?쓣 留뚮뱾怨? 5媛쒖쓽 ?옖?뜡媛?(0~ 9)?쓣 吏묒뼱?꽔?뼱 異쒕젰?븯?뒗 ?봽濡쒓렇?옩?쓣 ?옉?꽦?빐二쇱꽭?슂. 
-//	  - (List ?궗?슜媛??뒫)
+//	3. 정수형 배열을 만들고 5개의 랜덤값(0~ 9)을 집어넣어 출력하는 프로그램을 작성해주세요. 
+//	  - (List 사용가능)
 //	public static void main(String[] args) {
 //		List<Integer> a = new ArrayList<Integer>();
 //		for(int i =0; i<5;i++) {
@@ -45,7 +45,7 @@ public class KbgExam20180726 {
 //		}
 //	}
 
-//	4. ?젙?닔?삎 ?뜲?씠?꽣???엯 2媛쒕?? ?뙆?씪誘명꽣濡? 諛쏆븘 ?빀?쓣 由ы꽩?븯?뒗 硫붿냼?뱶瑜? ?옉?꽦?빐二쇱꽭?슂.
+//	4. 정수형 데이터타입 2개를 파라미터로 받아 합을 리턴하는 메소드를 작성해주세요.
 //	
 //	public int test(int a,int b) {
 //		
@@ -53,7 +53,7 @@ public class KbgExam20180726 {
 //	}
 
 //
-//	5. 1遺??꽣 50源뚯? 以묒뿉?꽌 3?쓽 諛곗닔?씠怨? 7?쓽諛곗닔?씤 ?닽?옄瑜? 異쒕젰?빐二쇱꽭?슂. 
+//	5. 1부터 50까지 중에서 3의 배수이고 7의배수인 숫자를 출력해주세요. 
 //	public static void main(String[] args) {
 //		for(int i = 1;i<50;i++) {
 //			if(i%3==0&&i%7==0) {
@@ -64,9 +64,9 @@ public class KbgExam20180726 {
 //	}
 	
 
-//	6. ?뙆?씪誘명꽣濡? ?엫?쓽?쓽 媛믪씠 ?뱾?뼱?엳?뒗 ?젙?닔?삎 諛곗뿴?쓣 諛쏆븘 理쒕?媛믪쓣 由ы꽩?빐二쇰뒗 硫붿냼?뱶瑜? ?옉?꽦?빐二쇱꽭?슂. 
-//	  - (?궗?씠利? 3 ~ 10)      
-//	  - (List ?궗?슜媛??뒫)
+//	6. 파라미터로 임의의 값이 들어있는 정수형 배열을 받아 최대값을 리턴해주는 메소드를 작성해주세요. 
+//	  - (사이즈 3 ~ 10)      
+//	  - (List 사용가능)
 //	public int test(int[] a) {
 //		int max = 0;
 //		for(int i=0;i<a.length;i++) {
@@ -85,8 +85,8 @@ public class KbgExam20180726 {
 //		return max;
 //	}
 //
-//	7. ?뙆?씪誘명꽣濡? String 諛곗뿴 怨? 臾몄옄?뿴?쓣 諛쏆븘 ?빐?떦 諛곗뿴?뿉 紐뉖쾲吏? 諛⑹뿉 ?빐?떦 臾몄옄?뿴?씠 ?엳?뒗吏? 由ы꽩?븯?뒗 硫붿냼?뱶瑜? ?옉?꽦?빐二쇱꽭?슂.
-//	  - (List ?궗?슜媛??뒫)
+//	7. 파라미터로 String 배열 과 문자열을 받아 해당 배열에 몇번째 방에 해당 문자열이 있는지 리턴하는 메소드를 작성해주세요.
+//	  - (List 사용가능)
 //	public int test(String[] a,String b) {
 //		for(int i = 0;i<a.length;i++) {
 //			if(a[i].equals(b)){
@@ -98,12 +98,12 @@ public class KbgExam20180726 {
 //		
 //	}
 //
-//	8. ?떎?쓬 ?봽濡쒓렇?옩?뿉 ?떎?뻾 寃곌낵瑜? ?벐?꽭?슂.
+//	8. 다음 프로그램에 실행 결과를 쓰세요.
 //
 //	class Father{
 //		
 //		public void run() {
-//			System.out.println("?븘鍮좉? ?떖由쎈땲?떎.");
+//			System.out.println("아빠가 달립니다.");
 //		}
 //	}
 //
@@ -111,7 +111,7 @@ public class KbgExam20180726 {
 //
 //		@Override
 //		public void run() {
-//			System.out.println("?궡媛? ?떖由쎈땲?떎.");
+//			System.out.println("내가 달립니다.");
 //		}
 //
 //		public static void main(String[] args) {
@@ -123,19 +123,19 @@ public class KbgExam20180726 {
 //		}
 //
 //	} 
-//	?궡媛? ?떖由쎈땲?떎.
-//	?븘鍮좉? ?떖由쎈땲?떎.
+//	내가 달립니다.
+//	아빠가 달립니다.
 
 
-//	9. user_info ?씪?뒗 ?씠由?, ?굹?씠, 二쇱냼瑜? ???옣?븷 ?닔 ?엳?뒗 table?쓣 留뚮뱶?뒗 紐낅졊?뼱瑜? ?벐?꽭?슂 
-//	  - (?씠由?, ?굹?씠?뒗 null媛믪씠 ?븞?릺寃? ?꽕?젙)
-//	  - (?뜲?씠?꽣 ???엯, ?궗?씠利덈뒗 ?쟻?떦?엳)
-//	  - (oracle, mariadb ?몮以묒뿉 ?븘臾닿굅?굹)
+//	9. user_info 라는 이름, 나이, 주소를 저장할 수 있는 table을 만드는 명령어를 쓰세요 
+//	  - (이름, 나이는 null값이 안되게 설정)
+//	  - (데이터 타입, 사이즈는 적당히)
+//	  - (oracle, mariadb 둘중에 아무거나)
 //	create table user_info(
-//	uiName varchar(200) not null,
+//	uiName varchar(20) not null,
 //	uiAge int not null,
 //	uiAddress varchar(1000) not null);
 //
-//	10. 諛⑷툑留뚮뱺 user_info ?뀒?씠釉붿쓽 ?궡?슜?쓣 紐⑤몢 蹂닿퀬 ?떢?쓣?븣?뿉 ?궗?슜?븯?뒗 紐낅졊?뼱瑜? ?벐?꽭?슂.
+//	10. 방금만든 user_info 테이블의 내용을 모두 보고 싶을때에 사용하는 명령어를 쓰세요.
 //	select * from user_info;
 }
